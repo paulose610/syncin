c44ee7f21bfaede4efc22b55c24cf6aa77d8bb1f
paulose610 <paulose610@gmail.com>
2024-03-06 11:41:27 +0530

    created frontend and backend

---
+++ frontend/src/components/navbar.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>this is navbar</h1>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <navbar />
+    <p>wlecome to home page</p>
+  </div>
+</template>
+
+<script setup>
+// @ is an alias to /src
+import navbar from "@/components/navbar.vue";
+
+</script>
